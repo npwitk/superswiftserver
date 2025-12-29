4342f9e33a874f6697224576b77a19a312ee84db
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-12-29 22:57:05 +0700

    Integrate Fluent in Vapor project

--- Sources/superswiftserver/configure.swift
+++ Sources/superswiftserver/configure.swift
@@ -1,10 +1,14 @@
 import Vapor
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    app.databases.use(.sqlite(.memory), as: .sqlite)
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     // register routes
     try routes(app)
+    
 }
